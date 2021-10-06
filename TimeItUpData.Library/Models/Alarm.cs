9c873ddec1cee8f99ba2659f1cf7952a91318f14
Łukasz Czepielik <koragrex@gmail.com>
2021-10-06 18:02:27 +0200

    Added empty Model and Repository class structures.

---
+++ TimeItUpData.Library/Models/Alarm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeItUpData.Library.Models
+{
+    public class Alarm
+    {
+        public string Id { get; set; }
+
+        public string TimerId { get; set; }
+        public virtual Timer Timer { get; set; }
+    }
+}
---
+++ TimeItUpData.Library/Models/Timer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeItUpData.Library.Models
+{
+    public class Timer
+    {
+        public string Id { get; set; }
+
+        public string UserId { get; set; }
+        public virtual User User { get; set; }
+
+        public virtual ICollection<Split> Splits { get; set; }
+        public virtual ICollection<Alarm> Alarms { get; set; }
+    }
+}
