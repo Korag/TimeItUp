22f5551eda23e519c6801583c7f5ff1d9a80a480
Łukasz Czepielik <koragrex@gmail.com>
2021-10-12 02:13:48 +0200

    Added PauseRepository class with relevant interface.

--- TimeItUpData.Library/Repositories/Pause/IPauseRepository.cs
+++ TimeItUpData.Library/Repositories/Pause/IPauseRepository.cs
@@ -1,6 +1,18 @@
-﻿namespace TimeItUpData.Library.Repositories
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using TimeItUpData.Library.Models;
+
+namespace TimeItUpData.Library.Repositories
 {
     public interface IPauseRepository
     {
+        Task<ICollection<Pause>> GetAllPausesAsync();
+        Task<ICollection<Pause>> GetAllActivePausesAsync();
+        Task<ICollection<Pause>> GetAllPastPausesAsync();
+        Task<Pause> GetPauseByIdAsync(int id);
+        Task<ICollection<Pause>> GetPausesByIdsAsync(ICollection<int> idSet);
+        void RemovePause(Pause pause);
+        Task AddPauseAsync(Pause pause);
+        bool CheckIfPauseExist(int id);
     }
 }
--- TimeItUpData.Library/Repositories/Pause/PauseRepository.cs
+++ TimeItUpData.Library/Repositories/Pause/PauseRepository.cs
@@ -1,4 +1,10 @@
-﻿using TimeItUpData.Library.DataAccess;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TimeItUpData.Library.DataAccess;
+using TimeItUpData.Library.Models;
 
 namespace TimeItUpData.Library.Repositories
 {
@@ -11,5 +17,44 @@ namespace TimeItUpData.Library.Repositories
             _context = context;
         }
 
+        public async Task<ICollection<Pause>> GetAllPausesAsync()
+        {
+            return await _context.Pauses.ToListAsync();
+        }
+
+        public async Task<ICollection<Pause>> GetAllActivePausesAsync()
+        {
+            return await _context.Pauses.Where(z => z.StartAt != DateTime.MinValue && z.EndAt == DateTime.MinValue).ToListAsync();
+        }
+
+        public async Task<ICollection<Pause>> GetAllPastPausesAsync()
+        {
+            return await _context.Pauses.Where(z => z.StartAt != DateTime.MinValue && z.EndAt != DateTime.MinValue).ToListAsync();
+        }
+
+        public async Task<Pause> GetPauseByIdAsync(int id)
+        {
+            return await _context.Pauses.Where(z => z.Id == id).FirstOrDefaultAsync();
+        }
+
+        public async Task<ICollection<Pause>> GetPausesByIdsAsync(ICollection<int> idSet)
+        {
+            return await _context.Pauses.Where(z => idSet.Contains(z.Id)).ToListAsync();
+        }
+
+        public void RemovePause(Pause pause)
+        {
+            _context.Pauses.Remove(pause);
+        }
+
+        public async Task AddPauseAsync(Pause pause)
+        {
+            await _context.Pauses.AddAsync(pause);
+        }
+
+        public bool CheckIfPauseExist(int id)
+        {
+            return _context.Pauses.Any(z => z.Id == id);
+        }
     }
 }
