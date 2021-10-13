9f479b47c2771f638c0ee38d09e478d9d50d78cf
Łukasz Czepielik <koragrex@gmail.com>
2021-10-13 13:40:37 +0200

    Updated TimeItUpData Db project.

--- TimeItUpData/dbo/Tables/Pauses.sql
+++ TimeItUpData/dbo/Tables/Pauses.sql
@@ -1,17 +1,18 @@
-﻿CREATE TABLE [dbo].[Pauses] (
-    [Id]            INT            NOT NULL,
-    [TimerId]       INT            NOT NULL,
-    [TimerId1]      INT            NOT NULL,
-    [TimerUserId]   NVARCHAR (450) NOT NULL,
-    [StartAt]       DATETIME2 (7)  NOT NULL,
-    [EndAt]         DATETIME2 (7)  NOT NULL,
-    [TotalDuration] NVARCHAR (30)  NOT NULL,
+CREATE TABLE [dbo].[Pauses] (
+    [Id]                    INT           IDENTITY (1, 1) NOT NULL,
+    [TimerId]               INT           NOT NULL,
+    [StartAt]               DATETIME2 (7) NOT NULL,
+    [EndAt]                 DATETIME2 (7) NOT NULL,
+    [TotalDuration]         NVARCHAR (30) NOT NULL,
+    [TotalDurationTimeSpan] TIME (7)      NOT NULL,
     CONSTRAINT [PK_Pauses] PRIMARY KEY CLUSTERED ([Id] ASC, [TimerId] ASC),
-    CONSTRAINT [FK_Pauses_Timers_TimerId1_TimerUserId] FOREIGN KEY ([TimerId1], [TimerUserId]) REFERENCES [dbo].[Timers] ([Id], [UserId]) ON DELETE CASCADE
+    CONSTRAINT [FK_Pauses_Timers_TimerId] FOREIGN KEY ([TimerId]) REFERENCES [dbo].[Timers] ([Id]) ON DELETE CASCADE
 );
 
 
+
+
 GO
-CREATE NONCLUSTERED INDEX [IX_Pauses_TimerId1_TimerUserId]
-    ON [dbo].[Pauses]([TimerId1] ASC, [TimerUserId] ASC);
+CREATE NONCLUSTERED INDEX [IX_Pauses_TimerId]
+    ON [dbo].[Pauses]([TimerId] ASC);
 
--- TimeItUpData/dbo/Tables/Timers.sql
+++ TimeItUpData/dbo/Tables/Timers.sql
@@ -1,19 +1,23 @@
 ﻿CREATE TABLE [dbo].[Timers] (
-    [Id]              INT            NOT NULL,
-    [UserId]          NVARCHAR (450) NOT NULL,
-    [Name]            NVARCHAR (100) NOT NULL,
-    [Description]     NVARCHAR (255) NULL,
-    [StartAt]         DATETIME2 (7)  NOT NULL,
-    [EndAt]           DATETIME2 (7)  NOT NULL,
-    [TotalDuration]   NVARCHAR (30)  NOT NULL,
-    [TotalPausedTime] NVARCHAR (MAX) NOT NULL,
-    [Paused]          BIT            NOT NULL,
-    [Finished]        BIT            NOT NULL,
-    [SplitsNumber]    INT            NOT NULL,
-    [AlarmsNumber]    INT            NOT NULL,
-    CONSTRAINT [PK_Timers] PRIMARY KEY CLUSTERED ([Id] ASC, [UserId] ASC),
-    CONSTRAINT [FK_Timers_Users_UserId] FOREIGN KEY ([UserId]) REFERENCES [dbo].[Users] ([Id]) ON DELETE CASCADE
+    [Id]                     INT            IDENTITY (1, 1) NOT NULL,
+    [UserId]                 NVARCHAR (450) NULL,
+    [Name]                   NVARCHAR (100) NOT NULL,
+    [Description]            NVARCHAR (255) NULL,
+    [StartAt]                DATETIME2 (7)  NOT NULL,
+    [EndAt]                  DATETIME2 (7)  NOT NULL,
+    [TotalDurationTimeSpan]  TIME (7)       NOT NULL,
+    [TotalPausedTimeSpan]    TIME (7)       NOT NULL,
+    [TotalCountdownTimeSpan] TIME (7)       NOT NULL,
+    [TotalDuration]          NVARCHAR (30)  NOT NULL,
+    [TotalPausedTime]        NVARCHAR (30)  NOT NULL,
+    [TotalCountdownTime]     NVARCHAR (30)  NOT NULL,
+    [Paused]                 BIT            NOT NULL,
+    [Finished]               BIT            NOT NULL,
+    CONSTRAINT [PK_Timers] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Timers_Users_UserId] FOREIGN KEY ([UserId]) REFERENCES [dbo].[Users] ([Id])
 );
+
+
 
 
 GO
