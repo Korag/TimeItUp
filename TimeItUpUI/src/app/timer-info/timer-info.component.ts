b6fb5d6dd2f1f79242508e8aeebb0dc552422d7b
Łukasz Czepielik <koragrex@gmail.com>
2021-10-27 02:27:31 +0200

    Added timer-info and timer-with-controls components.

--- TimeItUpUI/src/app/timer-info/timer-info.component.ts
+++ TimeItUpUI/src/app/timer-info/timer-info.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ToastrService } from 'ngx-toastr';
+import { RemoveTimerModalComponent } from '../remove-timer-modal';
+import { TimerModel } from '../_models';
+import { AuthService, TimerService, ValidationErrorPopulatorService } from '../_services';
 
 @Component({
   selector: 'app-timer-info',
@@ -6,10 +13,85 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./timer-info.component.scss']
 })
 export class TimerInfoComponent implements OnInit {
+  @Input() timer!: TimerModel;
+  updateTimerForm!: FormGroup;
+  loading = false;
+  formBlocked = true;
+  submitted = false;
+  reqErrors: any[] = [];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private formBuilder: FormBuilder,
+    private modalService: NgbModal,
+    private router: Router,
+    private timerService: TimerService,
+    private authService: AuthService,
+    private toastr: ToastrService,
+    private validHelp: ValidationErrorPopulatorService) {
   }
 
+  ngOnInit(): void {
+    this.updateTimerForm = this.formBuilder.group({
+      name: [{ value: this.timer.name, disabled: false }, Validators.compose([Validators.required, Validators.minLength(2), Validators.maxLength(100)])],
+      description: [{ value: this.timer.description, disabled: false }, Validators.compose([Validators.minLength(2), Validators.maxLength(255)])],
+    })
+  }
+
+  public get f() { return this.updateTimerForm.controls; }
+
+  async onSubmit() {
+    this.submitted = true;
+
+    if (this.updateTimerForm.invalid) {
+      this.toastr.error('The form contains incorrectly entered data');
+      return;
+    }
+
+    this.reqErrors = [];
+    this.formBlocked = true;
+    this.loading = true;
+
+    if ((this.f.name.value !== this.timer.name)
+      || (this.f.description.value !== this.timer.description)) {
+
+      try {
+        await this.timerService.updateTimerData(this.timer.id!,
+          this.f.name.value, this.f.description.value);
+
+        this.toastr.success('Updated timer data');
+
+      } catch (err) {
+        this.reqErrors = await this.validHelp.populateValidationErrorArray(err, this.reqErrors);
+
+        if (err.error.status === 404) {
+          this.reqErrors.push("The indicated timer does not exist.");
+          this.toastr.warning("Timer doesn't exist");
+        }
+      }
+    }
+
+    this.loading = false;
+  }
+
+  async removeTimer(): Promise<void> {
+    await this.timerService.removeTimer(this.timer.id!);
+    this.toastr.success('The timer has been removed');
+    this.router.navigate(["timers/active"]);
+  }
+
+  openRemoveTimerModal() {
+
+    const modalRef = this.modalService.open(RemoveTimerModalComponent,
+      {
+        scrollable: true,
+        windowClass: 'myCustomModalClass',
+        keyboard: false,
+        backdrop: 'static',
+        centered: false,
+        size: "modal-lg"
+      });
+    modalRef.componentInstance.deleteTimerEvent.subscribe(($e: any) => {
+      this.removeTimer();
+    });
+  }
 }
--- TimeItUpUI/src/app/timer-with-controls/timer-with-controls.component.ts
+++ TimeItUpUI/src/app/timer-with-controls/timer-with-controls.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { CountdownTimeModel, PauseModel, SplitModel, TimerModel } from '../_models';
+import { AuthService, PauseService, SplitService, TimerService } from '../_services';
 
 @Component({
   selector: 'app-timer-with-controls',
@@ -6,10 +10,124 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./timer-with-controls.component.scss']
 })
 export class TimerWithControlsComponent implements OnInit {
+  @Input() timer!: TimerModel;
+  @Output() finishTimerEvent = new EventEmitter<TimerModel>();
 
-  constructor() { }
+  countdownTime: CountdownTimeModel = new CountdownTimeModel();
+  intervalId!: any;
 
-  ngOnInit(): void {
+  isPaused!: boolean;
+  isStarted!: boolean;
+
+  pause!: PauseModel;
+  split!: SplitModel;
+
+  constructor(private router: Router,
+    private authService: AuthService,
+    private timerService: TimerService,
+    private pauseService: PauseService,
+    private splitService: SplitService,
+    private toastr: ToastrService) { }
+
+  async ngOnInit(): Promise<void> {
+    console.log(this.timer);
+    console.log(this);
+
+    if (this.timer.startAt?.toString() === "0001-01-01T00:00:00") {
+      this.isStarted = false;
+    }
+    else {
+      this.isStarted = true;
+    }
+
+    var countdownTimeSplitted = this.timer.totalCountdownTime?.split(":");
+    this.countdownTime.hours = parseInt(countdownTimeSplitted![0]);
+    this.countdownTime.minutes = parseInt(countdownTimeSplitted![1]);
+    this.countdownTime.seconds = parseInt(countdownTimeSplitted![2]);
+    this.countdownTime.miliseconds = parseInt(countdownTimeSplitted![3]);
+
+    this.isPaused = this.timer.paused!;
+
+    if (this.isPaused) {
+      this.pause = await this.pauseService.getTimerActivePause(this.timer.id!);
+    }
+    if (this.isStarted && !this.isPaused) {
+      this.split = await this.splitService.getTimerActiveSplit(this.timer.id!);
+
+      await this.startTimerCountdown();
+    }
   }
 
+  async runningCountdown() {
+    if (this.countdownTime.miliseconds !== 999) {
+      this.countdownTime.miliseconds!++;
+      return;
+    }
+    else {
+      if (this.countdownTime.seconds !== 59) {
+        this.countdownTime.seconds!++;
+        this.countdownTime.miliseconds! = 0;
+        return;
+      }
+      else {
+        if (this.countdownTime.minutes !== 59) {
+          this.countdownTime.minutes!++;
+          this.countdownTime.seconds! = 0;
+          this.countdownTime.miliseconds! = 0;
+          return;
+        }
+        else {
+          this.countdownTime.hours!++;
+          this.countdownTime.minutes! = 0;
+          this.countdownTime.seconds! = 0;
+          this.countdownTime.miliseconds! = 0;
+          return;
+        }
+      }
+    }
+  }
+
+  async startTimerCountdown() {
+    this.intervalId = setInterval(this.runningCountdown, 1);
+  }
+
+  async pauseTimerCountdown() {
+    clearInterval(this.intervalId);
+  }
+
+  async startTimer() {
+    await this.timerService.startTimer(this.timer.id!);
+    this.split = await this.splitService.getTimerActiveSplit(this.timer.id!);
+    this.toastr.success('The timer has been started');
+
+    await this.startTimerCountdown();
+  }
+
+  async finishTimer() {
+    this.finishTimerEvent.emit(this.timer);
+  }
+
+  async pauseTimer() {
+    this.pause = await this.pauseService.createPause(this.timer.id!);
+    await this.pauseService.startPause(this.pause.id!);
+    this.isPaused = true;
+    this.toastr.warning('The timer has been paused');
+
+    await this.pauseTimerCountdown();
+  }
+
+  async resumeTimer() {
+    await this.pauseService.finishPause(this.pause.id!);
+    this.isPaused = false;
+    this.toastr.info('The timer has been resumed');
+
+    await this.startTimerCountdown();
+  }
+
+  async createSplit() {
+    await this.splitService.finishSplit(this.split.id!);
+    this.split = await this.splitService.createSplit(this.timer.id!);
+    await this.splitService.startSplit(this.split.id!);
+    this.toastr.info('New split has been created');
+  }
 }
