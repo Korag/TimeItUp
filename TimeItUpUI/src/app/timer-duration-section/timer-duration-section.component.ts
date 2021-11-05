791d83f9208bc73ca92285e395f959cf57879759
Łukasz Czepielik <koragrex@gmail.com>
2021-11-06 00:52:44 +0100

    Added timer-duration-section component without dynamic control.

--- TimeItUpUI/src/app/timer-duration-section/timer-duration-section.component.ts
+++ TimeItUpUI/src/app/timer-duration-section/timer-duration-section.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { CountdownTimeModel, TimerModel } from '../_models';
 
 @Component({
   selector: 'app-timer-duration-section',
@@ -6,10 +7,97 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./timer-duration-section.component.scss']
 })
 export class TimerDurationSectionComponent implements OnInit {
+  @Input() timer!: TimerModel;
+  loading = false;
+
+  totalDurationTime: CountdownTimeModel = new CountdownTimeModel();
+  totalPauseTime: CountdownTimeModel = new CountdownTimeModel();
+
+  totalDurationIntervalId!: any;
+  totalPauseIntervalId!: any;
+
+  isPaused!: boolean;
+  isStarted!: boolean;
+  isFinished!: boolean;
 
   constructor() { }
 
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
+
+    if (this.timer.startAt?.toString() === "0001-01-01T00:00:00") {
+      this.isStarted = false;
+    }
+    else {
+      this.isStarted = true;
+    }
+
+    await this.calculateTime(this.totalDurationTime, this.timer.totalDuration);
+    await this.calculateTime(this.totalPauseTime, this.timer.totalPausedTime);
+
+    this.isPaused = this.timer.paused!;
+    this.isFinished = this.timer.finished!;
+
+    console.log(this.timer);
+
+    if (this.isPaused) {
+      await this.startPauseDurationCountdown();
+    }
+    if (this.isStarted && !this.isFinished) {
+      await this.startTotalDurationCountdown();
+    }
   }
 
+  async calculateTime(timeModel: CountdownTimeModel, timeMagicString: any) {
+    var timeSplitted = timeMagicString?.split(":");
+
+    timeModel.hours = parseInt(timeSplitted![0]);
+    timeModel.minutes = parseInt(timeSplitted![1]);
+    timeModel.seconds = parseInt(timeSplitted![2]);
+    timeModel.miliseconds = parseInt(timeSplitted![3]);
+  }
+
+  async runningTimeCountdown(countdownModel: CountdownTimeModel) {
+    if (countdownModel.miliseconds !== 999) {
+      countdownModel.miliseconds!++;
+      return;
+    }
+    else {
+      if (countdownModel.seconds !== 59) {
+        countdownModel.seconds!++;
+        countdownModel.miliseconds! = 0;
+        return;
+      }
+      else {
+        if (countdownModel.minutes !== 59) {
+          countdownModel.minutes!++;
+          countdownModel.seconds! = 0;
+          countdownModel.miliseconds! = 0;
+          return;
+        }
+        else {
+          countdownModel.hours!++;
+          countdownModel.minutes! = 0;
+          countdownModel.seconds! = 0;
+          countdownModel.miliseconds! = 0;
+          return;
+        }
+      }
+    }
+  }
+
+  async startTotalDurationCountdown() {
+    this.totalDurationIntervalId = setInterval(this.runningTimeCountdown.bind(this), 1, this.totalDurationTime);
+  }
+
+  async pauseTotalDurationCountdown() {
+    clearInterval(this.totalDurationIntervalId);
+  }
+
+  async startPauseDurationCountdown() {
+    this.totalPauseIntervalId = setInterval(this.runningTimeCountdown.bind(this), 1, this.totalPauseTime);
+  }
+
+  async pausePauseDurationCountdown() {
+    clearInterval(this.totalPauseIntervalId);
+  }
 }
